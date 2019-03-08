132c8f5c51d9a510958920e592d10951ea10c401
fantasy150592 <pdphuongit@gmail.com>
2019-03-08 22:47:04 +0700

    change 8/3

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,36 @@
+//hide preloader
+
 window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'))
+
+
+
+
+// reveal pattern
+// life immediately invoked function expression
+
+
+//function declaration
+function sayHi() {
+    console.log('hello');
+}
+
+//invoke the function 
+// sayHi();
+
+// function expression
+var person = function(name) {
+    console.log(`Hello I'm ${name}`);
+};
+
+//call the function
+// person('john');
+
+//immediately invoked function 
+// does not need to be invoked separately
+// variables not is global scope
+(function() {
+    //private variable not in the global scope
+    // var name = 'john';
+    // console.log(name);
+    // console.log('hello');
+})
