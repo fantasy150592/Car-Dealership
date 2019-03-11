1b5b91533910c96620f985c2c3429559cd1fcba8
fantasy150592 <pdphuongit@gmail.com>
2019-03-11 23:03:44 +0700

    finish 11/3

--- javascript/script.js
+++ javascript/script.js
@@ -74,13 +74,87 @@ const DisplaySpecialCars = ((CreateCars) => {
                 <h5 class="font-weight-bold mx-1">${item.make}</h5>
                 <h5 class="mx-1">${item.model}</h5>
             </div>
-            <!-- end of single item -->`;
-        });
+            <!-- end of single item -->`
+        })
         info.innerHTML = data;
+    })
+
+    //change img
+    info.addEventListener('click', (event) => {
+
+        if (event.target.parentElement.classList.contains('featured-icon')) {
+            //console.log('great');
+            const img = event.target.parentElement.dataset.img;
+            document.querySelector('.featured-photo').src = img;
+        }
     });
 
+})(CreateCars);
+
+const DisplayCars = ((CreateCars) => {
+    // all cars
+    const cars = CreateCars.cars;
+    console.log(cars);
+    // all container
+    const inventory = document.querySelector('.inventory-container');
+
+    // content loaded
+    document.addEventListener('DOMContentLoaded', () => {
+        inventory.innerHTML = '';
+        let output = '';
+        cars.forEach((car) => {
+            output += `<!-- single car -->
+            <div class="col-10 mx-auto my-3 col-md-6 col-lg-4 single-car ${car.country}">
+                <div class="card car-card">
+                    <img src="${car.img}" class="card-img-top car-img" alt="">
+                    <!-- car body -->
+                    <div class="car-body">
+                        <div class="car-info d-flex justify-content-between">
+                            <!-- first flex child -->
+                            <div class="car-text text-uppercase">
+                                <h6 class="font-weight-bold">${car.make}</h6>
+                                <h6>${car.model}</h6>
+                            </div>
+                            <!-- second flex child -->
+                            <h5 class="car-value align-self-center py-2 px-3">$
+                                <span class="car-price">${car.price}</span>
+                            </h5>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+                    <div class="card-footer text-capitalize d-flex justify-content-between">
+                        <p><span><i class="fas fa-car"></i></span>${car.type}</p>
+                        <p><span><i class="fas fa-cogs"></i></span>${car.trans}</p>
+                        <p><span><i class="fas fa-gas-pump"></i></span>${car.gas}</p>
+                    </div>
+                </div>
+            </div>
+            <!--end of single car -->`
+        })
+        inventory.innerHTML = output;
+    })
 
 })(CreateCars);
+
+// filter cars
+const FilterCars = (() => {
+    const filter = document.querySelectorAll('.filter-btn');
+    filter.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            const value = event.target.dataset.filter;
+            const singleCar = document.querySelectorAll('.single-car');
+            //console.log(singleCar);
+            singleCar.forEach((car) => {
+                if (value === 'all') {
+                    car.style.display = 'block';
+                } else {
+                    (!car.classList.contains(value)) ? car.style.display = 'none': car.style.display = 'block';
+                }
+            })
+        })
+    })
+})();
+//show model
 // console.log(CreateCars.cars);
 // console.log(CreateCars.specialCars);
 
